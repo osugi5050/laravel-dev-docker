88f98d4afe36b3c87c3285ec488e06e1594eca04
sugino tomoyuki <osugi.koko@gmail.com>
2019-03-28 18:26:30 +0900

    メニューAPI作成
    php artisan route:listでルーティングを確認

---
+++ server/app/Http/Controllers/Api/MenusController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Menu;
+
+class MenusController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $menus = Menu::all();
+        return $menus;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $menu = new Menu();
+        $menu->name = $request->name;
+        $menu->body = $request->body;
+        $menu->save();
+        return redirect('api/menus');    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $menu = Menu::find($id);
+        return $menu;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $menu = Menu::find($id);
+        $menu->name = $request->name;
+        $menu->body = $request->body;
+        $menu->save();
+        return redirect("api/menus/".$id);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $menu = Menu::find($id);
+        $menu->delete();
+        return redirect('api/menus');
+    }
+}
